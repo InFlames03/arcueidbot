d648a24474d60edc97bfb60993c1eea11dde37ec
Sai <saiprasanna@me.com>
2016-03-28 22:26:32 -0400

    Update arcbaka.py

--- plugins/arcbaka.py
+++ plugins/arcbaka.py
@@ -2,7 +2,7 @@ from telepot.namedtuple import InlineQueryResultArticle
 
 name = "baka"
 description = "Simple baka plugin"
-helpStr = "Says you are baka"
+helpStr = "Says you are a baka"
 usage = "/baka"
 regex = ["/baka"]
 regexInline = ["/baka"]
@@ -10,7 +10,7 @@ regexInline = ["/baka"]
 
 def handler(bot, msg, fullMsg, type):
     if type == "normal":
-        return "You  baka!"
+        return "You baka!"
     if type == "inline_query":
-        articles = [InlineQueryResultArticle(id='xyz', title='BAKA!', message_text='YOU ARE STILL BAKA')]
+        articles = [InlineQueryResultArticle(id='xyz', title='BAKA!', message_text='YOU ARE STILL A BAKA')]
         return articles
